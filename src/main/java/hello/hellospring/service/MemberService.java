8001791afc0ca6bce90772dce8d2f052179bd6cd
daniel <choral7451@gmail.com>
2022-12-06 07:52:58 +0900

    Add. 스프링 입문 마무리

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -40,16 +40,7 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
-
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
